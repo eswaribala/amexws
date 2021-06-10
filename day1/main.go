b9ba12b7fa66b193a3ab7b4eff778ae95d4a2ad3
eswaribala <parameswaribala@gmail.com>
2021-06-10 17:15:10 +0530

    bankinfo

--- day1/main.go
+++ day1/main.go
@@ -1,30 +1,23 @@
 package main
 
-//stdlib
 import (
 	"fmt"
-	"reflect"
-	"strconv"
+	"os"
 )
 
 //main frame is created stack
 func main() {
-	//explicitly typed
-	//var bankName string="ABC Bank";
-	//auto inferred
-	bankName := "ABC Bank"; //initialization
-	bankName="AMEX"; //reuse
-    var city,state="Chennai","TN";
-    //formatters
-    fmt.Printf("The Bank %s in the city %s and state %s\n",bankName,city,state);
-    balance:="50000";
-    var convBalance,err=strconv.Atoi(balance);
-    //data type checking
-    fmt.Println(reflect.TypeOf(convBalance).Kind());
- //conditional statements
-    if err==nil {
-		fmt.Printf("The numerical value of balance=%d\n", convBalance);
-	}else{
-		fmt.Printf("The associated error%s\n", err);
+	createBankInfo()
+	for index, customer := range createCustomers() {
+		fmt.Printf("Position is %d and Name=%s ", index, customer)
 	}
+	//readTransactionData();
+	generateIds()
+	result, err := dbConfiguration(os.Args)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(result)
+	}
+
 }
