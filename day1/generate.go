4c3564bf43046f30fc6a35419252b11fd9f60c8d
eswaribala <parameswaribala@gmail.com>
2021-06-10 15:56:38 +0530

    bankinfo

---
+++ day1/generate.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func generateIds() {
+	//create slice
+	ids := make([]int32, 50)
+	//assign values
+	for id := range ids {
+		ids[id] = rand.Int31()
+	}
+	//loop over
+	for index, id := range ids {
+		fmt.Println(index, "=>", id)
+	}
+
+}
