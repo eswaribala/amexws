c891aabeb4d121c679f9fe97a676beb329299012
eswaribala <parameswaribala@gmail.com>
2021-06-11 13:18:45 +0530

    bankinfo

--- banking/models/customer.go
+++ banking/models/customer.go
@@ -1,5 +1,7 @@
 package models
 
+import "fmt"
+
 type Address struct {
 	Door_No     string
 	Street_Name string
@@ -14,4 +16,25 @@ type Customer struct {
 	Contact_Number int64
 	Email          string
 	Password       string
+}
+
+/**
+
+Method has receiver type
+*/
+//update the address
+//call by reference
+func (customer *Customer) UpdateAddress(address *Address) {
+
+	(*customer).AddressRef = *address
+}
+
+//update the contact no
+//call by value
+//receiver type,parameter list,return type
+func (customer Customer) UpdateContactNo(contact_no int64) Customer {
+
+	customer.Contact_Number = contact_no
+	fmt.Printf("\nInternal Customer Details %+v", customer)
+	return customer
 }
--- banking/utility/app.go
+++ banking/utility/app.go
@@ -2,9 +2,39 @@ package main
 
 import (
 	"fmt"
-	"github.com/amexws/banking/dao"
+	dao "github.com/amexws/banking/dao"
+	models "github.com/amexws/banking/models"
+	"math/rand"
 )
 
 func main() {
 	fmt.Println(dao.FindAllCustomers())
+	fmt.Printf("\nCustomer Data %+v", dao.FindAllCustomers())
+	//partial customer struct
+
+	customerObj := dao.FindAllCustomers()
+	//inline structure
+	partial_customer := struct {
+		account_no int64
+		name       string
+	}{
+		account_no: rand.Int63(),
+		name:       customerObj.Name,
+	}
+
+	fmt.Printf("\nPartial Customer Data%+v", partial_customer)
+
+	//update the address
+	address := models.Address{"487658", "Gandhi st", "Bangalore", "KN"}
+	fmt.Printf("\nAddress %+v", address)
+	// how do we invoke method?
+	//use always instance to access method which is receiver type
+	customer_response := dao.FindAllCustomers()
+	customer_response.UpdateAddress(&address)
+
+	fmt.Printf("\nAfter Address Update Customer Data %+v", customer_response)
+
+	customer_response = customer_response.UpdateContactNo(555555500)
+	fmt.Printf("\nAfter Contact Number Update Customer Data %+v", customer_response)
+
 }
