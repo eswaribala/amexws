3db375e3663cad0743d1249d3e9923634b9c54b7
eswaribala <parameswaribala@gmail.com>
2021-06-12 12:39:52 +0530

    bankinfo

--- routineandchannels/main.go
+++ routineandchannels/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"sync"
 )
@@ -17,24 +18,37 @@ func main() {
 		"http://rediffmail.com",
 		"http://rpsconsulting.in",
 	}
-
+	//add counter size
 	wg.Add(len(links))
 	for _, link := range links {
 		//add go routine
 		go CheckLinks(link)
 	}
+	//make main to wait
 	wg.Wait()
 	fmt.Println("Main Terminated....")
 }
 
 func CheckLinks(link string) {
-
-	_, err := http.Get(link)
+	//signal main that job done
+	defer wg.Done()
+	response, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "The site is down")
 
-	} else {
-		fmt.Println(link, "The site is up")
 	}
 
+	go CheckUrl(link)
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Error Occurred in reading", err)
+	}
+
+	fmt.Println(len(body))
+
+}
+
+func CheckUrl(url string) {
+	fmt.Println("Received Url", url)
 }
